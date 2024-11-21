af01b18ba87783220c95ee02ab4f39bbe8797fa7
akmalkhairy <akmalkhairy90@gmail.com>
2024-11-21 19:24:22 +0800

    Version 3

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 import { getPizzaDetails } from "./indexx";
 
-conslo
+const menu = [
+  { name: "Magherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
 
-// const menu = [
-//   { name: "Magherita", price: 8 },
-//   { name: "Pepperoni", price: 10 },
-//   { name: "Hawaiian", price: 10 },
-//   { name: "Veggie", price: 9 },
-// ];
+let cashInRegister = 100;
+const orderQueue = [];
+let id = 0;
 
-// let cashInRegister = 100;
-// const orderQueue = [];
-// let id = 0;
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+  console.log(menu);
+}
 
-// function addNewPizza(pizzaObj) {
-//   menu.push(pizzaObj);
-//   console.log(menu);
-// }
+addNewPizza({ name: "One Pizza", price: 8 });
+addNewPizza({ name: "One Fav", price: 8 });
 
-// addNewPizza({ name: "One Pizza", price: 8 });
-// addNewPizza({ name: "One Fav", price: 8 });
+function placeOrder(pizzaName) {
+  menu.forEach((menu) => {
+    if (menu.name === pizzaName) {
+      cashInRegister += menu.price;
+      console.log(cashInRegister);
+      const newOrder = { pizza: menu.name, status: "ordered", id: id++ };
+      orderQueue.push(newOrder);
+      console.log(orderQueue);
+      return newOrder;
+    }
+  });
+}
 
-// function placeOrder(pizzaName) {
-//   menu.forEach((menu) => {
-//     if (menu.name === pizzaName) {
-//       cashInRegister += menu.price;
-//       console.log(cashInRegister);
-//       const newOrder = { pizza: menu.name, status: "ordered", id: id++ };
-//       orderQueue.push(newOrder);
-//       console.log(orderQueue);
-//       return newOrder;
-//     }
-//   });
-// }
+placeOrder("One Pizza");
+placeOrder("Magherita");
 
-// placeOrder("One Pizza");
-// placeOrder("Magherita");
+function completeOrder(orderId) {
+  orderQueue.forEach((order) => {
+    if (order.id === orderId) {
+      order.status = "completed";
+      console.log(orderQueue);
+      return order;
+    }
+  });
+}
 
-// function completeOrder(orderId) {
-//   orderQueue.forEach((order) => {
-//     if (order.id === orderId) {
-//       order.status = "completed";
-//       console.log(orderQueue);
-//       return order;
-//     }
-//   });
-// }
-
-// completeOrder(1);
+completeOrder(1);
